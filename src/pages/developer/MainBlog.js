fecbafc0f62d08a72b43072b2574cd2593cb642f
nirrvana <wlwhs91@gmail.com>
2020-02-04 20:30:17 +0900

    Build structure

---
+++ src/pages/developer/MainBlog.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MainBlog extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
---
+++ src/pages/developer/MyPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyPage extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
