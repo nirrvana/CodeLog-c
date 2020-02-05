fe66b218297438f451c0639e7bc60915d8d9aa98
nirrvana <wlwhs91@gmail.com>
2020-02-05 20:29:25 +0900

    Add App

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
-// * Component
-import SignUpPage from './pages/SignUpPage';
-import HomePage from './pages/HomePage';
+import { connect } from 'react-redux';
 import SignIn from './pages/SignIn';
-import Tab from './pages/Tab';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
 
-// * CSS
-import 'antd/dist/antd.css';
-import './components/signup/SignUp.css';
-import './pages/HomePage.css';
-import './pages/Tab.css';
-
-export default class App extends Component {
+class App extends Component {
   render() {
-    return (
-      <div>
-        <Router>
-          <Route path="/" exact component={() => <HomePage></HomePage>}></Route>
+    const { token } = this.props;
+    console.log(1004, token)
+    if (!token) {
+      return (
+        <BrowserRouter>
           <Switch>
-            <Route path="/SignIn" component={() => <SignIn></SignIn>}></Route>
-            <Route
-              path="/SignUp"
-              component={() => <SignUpPage></SignUpPage>}
-            ></Route>
+            <Route exact path="/" component={SignIn} />
+            <Redirect to="/" />
           </Switch>
-        </Router>
-      </div>
-    );
+        </BrowserRouter>
+      );
+    } else {
+      return (
+        <div>hello</div>
+      )
+    }
   }
 }
+
+const mapStateToProps = state => ({ 
+  token: state.session.token,
+});
+
+export default connect(mapStateToProps)(App);
