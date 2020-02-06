91271d91bbe27ed58236c92a4da51c33d7390897
이은지 <eunji.lee@ieunjiui-MacBook-Pro.local>
2020-02-06 21:12:19 +0900

    Add Like function & Post link function for post

--- src/components/mainblog/techpost/TechPostListElement.js
+++ src/components/mainblog/techpost/TechPostListElement.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Router, Link, Redirect } from 'react-router-dom';
 
 export default class TechPostListElement extends Component {
   constructor(props) {
@@ -8,8 +9,10 @@ export default class TechPostListElement extends Component {
     const { data } = this.props;
     return (
       <div className="cl_PostListElement">
-        <div className="cl_PostListElement_Title">{data.title}</div>
-        <div>{data.body}</div>
+        <Link to="/TechPost">
+          <div className="cl_PostListElement_Title"> {data.title}</div>
+          <div className="cl_PostListElement_Contents">{data.body}</div>
+        </Link>
       </div>
     );
   }
