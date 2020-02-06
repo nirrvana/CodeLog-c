18500c8246150d0b8ffa0d9e4916ef5f7bbcdbd0
nirrvana <wlwhs91@gmail.com>
2020-02-06 19:10:34 +0900

    Add Mypage

--- src/pages/developer/MyPage.js
+++ src/pages/developer/MyPage.js
@@ -1,11 +1,45 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+//# redux
+import { mypage } from '../../redux/action';
+import { getMyPageData } from '../../redux/api';
+//# component
+import Tab from '../../components/mypage/Tab';
+import PostTagCount from '../../components/mypage/PostTagCount';
+import UserInfo from '../../components/mypage/UserInfo';
+import RecommendedCompanyList from '../../components/mypage/RecommendedCompanyList';
+import fakeData from '../../components/home/fakedata';
+import './MyPage.css';
 
-export default class MyPage extends Component {
+
+class MyPage extends Component {
+  componentDidMount() {
+    this.props.receiveData();
+  }
+
   render() {
     return (
-      <div>
-        
+      <div className="cl_MyPage">
+        <Tab />
+        <PostTagCount />
+        <UserInfo />
+        <h1 className="cl_Company_Title">로 추천된 ***님의 파트너사는</h1>
+        <RecommendedCompanyList fakeData={fakeData} />
       </div>
-    )
+    );
   }
 }
+
+const mapStateToProps = (state) => ({
+  token: state.session.token,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  receiveData: (token) => (event) => {
+    getMyPageData(token).then(({ data: { post_count, tag_count } }) => {
+      dispatch(mypage(post_count, tag_count));
+    });
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(MyPage);
