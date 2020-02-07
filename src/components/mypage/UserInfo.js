c3cc9d199e5b22478c3d739f50c6fb3a080e0bab
nirrvana <wlwhs91@gmail.com>
2020-02-07 19:58:06 +0900

    Adjust css

--- src/components/mypage/UserInfo.js
+++ src/components/mypage/UserInfo.js
@@ -5,9 +5,15 @@ export default class UserInfo extends Component {
   render() {
     return (
       <div className="cl_UserInfo">
-        <Link to="/Resume">이력서</Link>
-        <Link to="/CoverLetter">자기소개서</Link>
-        <Link to="/Portfolio">포트폴리오</Link>
+        <div className="cl_ApplySheet">
+          <Link to="/Resume">이력서</Link>
+        </div>
+        <div className="cl_ApplySheet">
+          <Link to="/CoverLetter">자기소개서</Link>
+        </div>
+        <div className="cl_ApplySheet">
+          <Link to="/Portfolio">포트폴리오</Link>
+        </div>
       </div>
     );
   }
