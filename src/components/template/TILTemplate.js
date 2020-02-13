fa02e9ca4e01b07b3b2e10a9f1f7469b5e65c107
nirrvana <wlwhs91@gmail.com>
2020-02-14 00:48:08 +0900

    Add a til template

---
+++ src/components/template/TILTemplate.js
@@ -0,0 +1,191 @@
+import React, { Component } from 'react';
+import ReactMarkdown from 'react-markdown';
+import TextareaAutosize from 'react-textarea-autosize';
+import { Tag, Input, Button, Avatar, AutoComplete, List } from 'antd';
+import CodeBlock from '../../components/postedit/CodeBlock';
+import { getTags, postTILPost } from '../../redux/api';
+
+export default class TILTemplate extends Component {
+  state = {
+    theme: 'til',
+    title: '',
+    fact: '',
+    feeling: '',
+    finding: '',
+    future_action: '',
+    tags: [],
+    selected_tags: [],
+  };
+
+  componentDidMount() {
+    getTags()
+      .then(({ data }) =>
+        this.setState({
+          tags: data,
+        }),
+      )
+      .catch((err) => console.log('태그목록을 받아오지 못하였습니다.'));
+  }
+
+  handleInputChange = (state) => ({ target: { value: input } }) => {
+    this.setState({
+      [state]: input,
+    });
+  };
+
+  selectTag = (e) => {
+    const { selected_tags } = this.state;
+    const target = e.target.innerText;
+    if (!selected_tags.includes(target)) {
+      e.target.style['background-color'] = 'blue';
+      this.setState({
+        selected_tags: [...selected_tags, target],
+      });
+      console.log(selected_tags);
+    } else {
+      e.target.style['background-color'] = 'gray';
+      this.setState({
+        selected_tags: selected_tags.filter((tag) => tag !== target),
+      });
+      console.log(selected_tags);
+    }
+  };
+
+  handleSubmit = (e) => {
+    const {
+      theme,
+      title,
+      fact,
+      feeling,
+      finding,
+      future_action,
+      selected_tags,
+    } = this.state;
+    console.log(
+      111,
+      theme,
+      title,
+      theme,
+      title,
+      fact,
+      feeling,
+      finding,
+      future_action,
+      selected_tags,
+    );
+
+    const content = `${fact}${feeling}${finding}${future_action}`;
+    postTILPost(theme, title, content, selected_tags)
+      .then((res) => window.alert('post successfully!'))
+      .catch((err) => window.alert('fail to post..'));
+  };
+
+  render() {
+    const { fact, feeling, finding, future_action, tags } = this.state;
+    return (
+      <div>
+        <div className="cl_Post">
+          <Input
+            className="cl_Edit_Title cl_Post_set "
+            type="text"
+            onChange={this.handleInputChange('title')}
+            defaultValue={'title'}
+          />
+          <div className="cl_Post_author_Info cl_Post_set ">
+            <Avatar
+              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+              alt="Han Solo"
+            />
+            <div className="cl_Post_author">Root</div>
+          </div>
+          <div className="cl_Post_Contents ">
+            <div className="cl_Post_Edit_Subtitle ">Fact</div>
+            <div className="cl_Plain_Edit_Content ">
+              <TextareaAutosize
+                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                onChange={this.handleInputChange('fact')}
+                defaultValue={'hello'}
+              />
+              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                <ReactMarkdown
+                  source={fact}
+                  renderers={{
+                    code: CodeBlock,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="cl_Post_Edit_Subtitle ">Feeling</div>
+            <div className="cl_Plain_Edit_Content ">
+              <TextareaAutosize
+                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                onChange={this.handleInputChange('feeling')}
+                defaultValue={'hello'}
+              />
+              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                <ReactMarkdown
+                  source={feeling}
+                  renderers={{
+                    code: CodeBlock,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="cl_Post_Edit_Subtitle ">Finding</div>
+            <div className="cl_Plain_Edit_Content ">
+              <TextareaAutosize
+                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                onChange={this.handleInputChange('finding')}
+                defaultValue={'hello'}
+              />
+              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                <ReactMarkdown
+                  source={finding}
+                  renderers={{
+                    code: CodeBlock,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="cl_Post_Edit_Subtitle ">Future action</div>
+            <div className="cl_Plain_Edit_Content ">
+              <TextareaAutosize
+                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                onChange={this.handleInputChange('future_action')}
+                defaultValue={'hello'}
+              />
+              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                <ReactMarkdown
+                  source={future_action}
+                  renderers={{
+                    code: CodeBlock,
+                  }}
+                />
+              </div>
+            </div>
+            <div className="cl_Post_Tags cl_Post_set">
+              <List
+                dataSource={tags}
+                renderItem={(item) => (
+                  <span>
+                    <Tag color="gray" onClick={this.selectTag}>
+                      {item}
+                    </Tag>
+                  </span>
+                )}
+              />
+            </div>
+            <Button
+              type="primary"
+              className="cl_Post_Comments_Add_Btn"
+              onClick={this.handleSubmit}
+            >
+              Post!
+            </Button>
+            <div className="cl_post_Margin"></div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
