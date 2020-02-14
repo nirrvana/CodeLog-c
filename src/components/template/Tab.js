075dfba144961ab82b09c110ccef01a66e76c440
nirrvana <wlwhs91@gmail.com>
2020-02-14 14:20:35 +0900

    Add tab in template page

--- src/components/template/Tab.js
+++ src/components/template/Tab.js
@@ -2,16 +2,59 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // * CSS
-import { Layout, Menu } from 'antd';
+import { Layout, Menu, Dropdown } from 'antd';
 const { Header } = Layout;
 
 export default class Tab extends Component {
   render() {
+    const post_type = (
+      <Menu>
+        <Menu.Item>
+          <a
+            rel="noopener noreferrer"
+            href="http://localhost:3000/WritePlainPost"
+          >
+            PlainPost
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            rel="noopener noreferrer"
+            href="http://localhost:3000/WriteDevPost"
+          >
+            DevPost
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            rel="noopener noreferrer"
+            href="http://localhost:3000/WriteTechPost"
+          >
+            TechPost
+          </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a
+            rel="noopener noreferrer"
+            href="http://localhost:3000/WriteTILPost"
+          >
+            TILPost
+          </a>
+        </Menu.Item>
+      </Menu>
+    );
     return (
       <Layout className="layout">
         <Header className="cl_Tab_Header">
           <Menu mode="horizontal" style={{ lineHeight: '64px' }}>
             <Menu.Item className="cl_Home_Logo">CODE | LOG</Menu.Item>
+            <Menu.Item>
+              <Dropdown overlay={post_type}>
+                <a className="ant-dropdown-link" href="#">
+                  New story
+                </a>
+              </Dropdown>
+            </Menu.Item>
             <Menu.Item>
               <Link to="/Blog">Blog</Link>
             </Menu.Item>
