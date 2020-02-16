30e65f4e8469b922a3d1f9095f368d8b8bd44ca5
nirrvana <wlwhs91@gmail.com>
2020-02-16 23:36:16 +0900

    Redirect to detail page after posting

--- src/components/template/TILTemplate.js
+++ src/components/template/TILTemplate.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+//* redux
+import { connect } from 'react-redux';
+import { getTags, postTILPost } from '../../redux/api';
+import { currentPost } from '../../redux/action';
+//* parser
 import ReactMarkdown from 'react-markdown';
 import TextareaAutosize from 'react-textarea-autosize';
-import { Tag, Input, Button, Avatar, AutoComplete, List } from 'antd';
 import CodeBlock from '../../components/postedit/CodeBlock';
-import { getTags, postTILPost } from '../../redux/api';
-
-export default class TILTemplate extends Component {
+//* css
+import { Tag, Input, Button, Avatar, List } from 'antd';
+class TILTemplate extends Component {
   state = {
     theme: 'til',
     title: '',
@@ -15,6 +20,7 @@ export default class TILTemplate extends Component {
     future_action: '',
     tags: [],
     selected_tags: [],
+    isPosted: false,
   };
 
   componentDidMount() {
@@ -76,116 +82,137 @@ export default class TILTemplate extends Component {
 
     const content = `${fact}${feeling}${finding}${future_action}`;
     postTILPost(theme, title, content, selected_tags)
-      .then((res) => window.alert('post successfully!'))
+      .then(({ data: { id } }) => {
+        this.props.handlePost(id);
+        window.alert('post successfully!');
+        this.setState({ isPosted: true });
+      })
       .catch((err) => window.alert('fail to post..'));
   };
 
   render() {
-    const { fact, feeling, finding, future_action, tags } = this.state;
-    return (
-      <div>
-        <div className="cl_Post">
-          <Input
-            className="cl_Edit_Title cl_Post_set "
-            type="text"
-            onChange={this.handleInputChange('title')}
-            defaultValue={'title'}
-          />
-          <div className="cl_Post_author_Info cl_Post_set ">
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
+    const {
+      fact,
+      feeling,
+      finding,
+      future_action,
+      tags,
+      isPosted,
+    } = this.state;
+    if (isPosted) {
+      return <Redirect to="TILPost" />;
+    } else {
+      return (
+        <div>
+          <div className="cl_Post">
+            <Input
+              className="cl_Edit_Title cl_Post_set "
+              type="text"
+              onChange={this.handleInputChange('title')}
+              defaultValue={'title'}
             />
-            <div className="cl_Post_author">Root</div>
-          </div>
-          <div className="cl_Post_Contents ">
-            <div className="cl_Post_Edit_Subtitle ">Fact</div>
-            <div className="cl_Plain_Edit_Content ">
-              <TextareaAutosize
-                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
-                onChange={this.handleInputChange('fact')}
-                defaultValue={'hello'}
+            <div className="cl_Post_author_Info cl_Post_set ">
+              <Avatar
+                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                alt="Han Solo"
               />
-              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
-                <ReactMarkdown
-                  source={fact}
-                  renderers={{
-                    code: CodeBlock,
-                  }}
+              <div className="cl_Post_author">Root</div>
+            </div>
+            <div className="cl_Post_Contents ">
+              <div className="cl_Post_Edit_Subtitle ">Fact</div>
+              <div className="cl_Plain_Edit_Content ">
+                <TextareaAutosize
+                  className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                  onChange={this.handleInputChange('fact')}
+                  defaultValue={'hello'}
+                />
+                <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                  <ReactMarkdown
+                    source={fact}
+                    renderers={{
+                      code: CodeBlock,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="cl_Post_Edit_Subtitle ">Feeling</div>
+              <div className="cl_Plain_Edit_Content ">
+                <TextareaAutosize
+                  className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                  onChange={this.handleInputChange('feeling')}
+                  defaultValue={'hello'}
+                />
+                <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                  <ReactMarkdown
+                    source={feeling}
+                    renderers={{
+                      code: CodeBlock,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="cl_Post_Edit_Subtitle ">Finding</div>
+              <div className="cl_Plain_Edit_Content ">
+                <TextareaAutosize
+                  className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                  onChange={this.handleInputChange('finding')}
+                  defaultValue={'hello'}
+                />
+                <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                  <ReactMarkdown
+                    source={finding}
+                    renderers={{
+                      code: CodeBlock,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="cl_Post_Edit_Subtitle ">Future action</div>
+              <div className="cl_Plain_Edit_Content ">
+                <TextareaAutosize
+                  className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                  onChange={this.handleInputChange('future_action')}
+                  defaultValue={'hello'}
+                />
+                <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                  <ReactMarkdown
+                    source={future_action}
+                    renderers={{
+                      code: CodeBlock,
+                    }}
+                  />
+                </div>
+              </div>
+              <div className="cl_Post_Tags cl_Post_set">
+                <List
+                  dataSource={tags}
+                  renderItem={(item) => (
+                    <span>
+                      <Tag color="gray" onClick={this.selectTag}>
+                        {item}
+                      </Tag>
+                    </span>
+                  )}
                 />
               </div>
+              <Button
+                type="primary"
+                className="cl_Post_Comments_Add_Btn"
+                onClick={this.handleSubmit}
+              >
+                Post!
+              </Button>
+              <div className="cl_post_Margin"></div>
             </div>
-            <div className="cl_Post_Edit_Subtitle ">Feeling</div>
-            <div className="cl_Plain_Edit_Content ">
-              <TextareaAutosize
-                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
-                onChange={this.handleInputChange('feeling')}
-                defaultValue={'hello'}
-              />
-              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
-                <ReactMarkdown
-                  source={feeling}
-                  renderers={{
-                    code: CodeBlock,
-                  }}
-                />
-              </div>
-            </div>
-            <div className="cl_Post_Edit_Subtitle ">Finding</div>
-            <div className="cl_Plain_Edit_Content ">
-              <TextareaAutosize
-                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
-                onChange={this.handleInputChange('finding')}
-                defaultValue={'hello'}
-              />
-              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
-                <ReactMarkdown
-                  source={finding}
-                  renderers={{
-                    code: CodeBlock,
-                  }}
-                />
-              </div>
-            </div>
-            <div className="cl_Post_Edit_Subtitle ">Future action</div>
-            <div className="cl_Plain_Edit_Content ">
-              <TextareaAutosize
-                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
-                onChange={this.handleInputChange('future_action')}
-                defaultValue={'hello'}
-              />
-              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
-                <ReactMarkdown
-                  source={future_action}
-                  renderers={{
-                    code: CodeBlock,
-                  }}
-                />
-              </div>
-            </div>
-            <div className="cl_Post_Tags cl_Post_set">
-              <List
-                dataSource={tags}
-                renderItem={(item) => (
-                  <span>
-                    <Tag color="gray" onClick={this.selectTag}>
-                      {item}
-                    </Tag>
-                  </span>
-                )}
-              />
-            </div>
-            <Button
-              type="primary"
-              className="cl_Post_Comments_Add_Btn"
-              onClick={this.handleSubmit}
-            >
-              Post!
-            </Button>
-            <div className="cl_post_Margin"></div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  handlePost: (id) => dispatch(currentPost(id)),
+});
+
+export default connect(null, mapDispatchToProps)(TILTemplate);
--- src/components/template/plainTemplate2.js
+++ src/components/template/plainTemplate2.js
@@ -1,18 +1,24 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+//* redux
+import { connect } from 'react-redux';
+import { getTags, postPlainPost } from '../../redux/api';
+import { currentPost } from '../../redux/action';
+//* parser
 import ReactMarkdown from 'react-markdown';
 import TextareaAutosize from 'react-textarea-autosize';
-import { Tag, Input, Button, Avatar, AutoComplete, List } from 'antd';
 import CodeBlock from '../../components/postedit/CodeBlock';
-import { getTags, postPlainPost } from '../../redux/api';
-import { getRandomInt, colorArray } from '../../TagColor';
+//* css
+import { Tag, Input, Button, Avatar, List } from 'antd';
 
-export default class PlainTemplate2 extends Component {
+class PlainTemplate2 extends Component {
   state = {
     theme: 'plain',
     title: '',
     content: '',
     tags: [],
     selected_tags: [],
+    isPosted: false,
   };
 
   componentDidMount() {
@@ -54,69 +60,83 @@ export default class PlainTemplate2 extends Component {
     console.log(111, theme, title, content, selected_tags);
 
     postPlainPost(theme, title, content, selected_tags)
-      .then((res) => window.alert('post successfully!'))
+      .then(({ data: { id } }) => {
+        this.props.handlePost(id);
+        window.alert('post successfully!');
+        this.setState({ isPosted: true });
+      })
       .catch((err) => window.alert('fail to post..'));
   };
 
   render() {
-    const { content, tags } = this.state;
-    return (
-      <div>
-        <div className="cl_Post">
-          <Input
-            className="cl_Edit_Title cl_Post_set "
-            type="text"
-            onChange={this.handleInputChange('title')}
-            defaultValue={'title'}
-          />
-          <div className="cl_Post_author_Info cl_Post_set ">
-            <Avatar
-              src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
-              alt="Han Solo"
+    const { content, tags, isPosted } = this.state;
+    if (isPosted) {
+      return <Redirect to="PlainPost" />;
+    } else {
+      return (
+        <div>
+          <div className="cl_Post">
+            <Input
+              className="cl_Edit_Title cl_Post_set "
+              type="text"
+              onChange={this.handleInputChange('title')}
+              defaultValue={'title'}
             />
-            <div className="cl_Post_author">Root</div>
-          </div>
-          <div className="cl_Post_Contents ">
-            <div className="cl_Post_Edit_Subtitle ">Content</div>
-            <div className="cl_Plain_Edit_Content ">
-              <TextareaAutosize
-                className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
-                onChange={this.handleInputChange('content')}
-                defaultValue={'hello'}
+            <div className="cl_Post_author_Info cl_Post_set ">
+              <Avatar
+                src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                alt="Han Solo"
               />
-              <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
-                <ReactMarkdown
-                  source={content}
-                  renderers={{
-                    code: CodeBlock,
-                  }}
+              <div className="cl_Post_author">Root</div>
+            </div>
+            <div className="cl_Post_Contents ">
+              <div className="cl_Post_Edit_Subtitle ">Content</div>
+              <div className="cl_Plain_Edit_Content ">
+                <TextareaAutosize
+                  className="cl_Plain_Edit_Text cl_Plain_Edit_Set"
+                  onChange={this.handleInputChange('content')}
+                  defaultValue={'hello'}
+                />
+                <div className="cl_Plain_Edit_Markdown cl_Plain_Edit_Set">
+                  <ReactMarkdown
+                    source={content}
+                    renderers={{
+                      code: CodeBlock,
+                    }}
+                  />
+                </div>
+              </div>
+
+              <div className="cl_Post_Tags cl_Post_set">
+                <List
+                  dataSource={tags}
+                  renderItem={(item) => (
+                    <span>
+                      <Tag color="gray" onClick={this.selectTag}>
+                        {item}
+                      </Tag>
+                    </span>
+                  )}
                 />
               </div>
+              <Button
+                type="primary"
+                className="cl_Post_Comments_Add_Btn"
+                onClick={this.handleSubmit}
+              >
+                Post!
+              </Button>
+              <div className="cl_post_Margin"></div>
             </div>
-
-            <div className="cl_Post_Tags cl_Post_set">
-              <List
-                dataSource={tags}
-                renderItem={(item) => (
-                  <span>
-                    <Tag color="gray" onClick={this.selectTag}>
-                      {item}
-                    </Tag>
-                  </span>
-                )}
-              />
-            </div>
-            <Button
-              type="primary"
-              className="cl_Post_Comments_Add_Btn"
-              onClick={this.handleSubmit}
-            >
-              Post!
-            </Button>
-            <div className="cl_post_Margin"></div>
           </div>
         </div>
-      </div>
-    );
+      );
+    }
   }
 }
+
+const mapDispatchToProps = (dispatch) => ({
+  handlePost: (id) => dispatch(currentPost(id)),
+});
+
+export default connect(null, mapDispatchToProps)(PlainTemplate2);
