96eec97e70ffba51a8c400dbb15c946e72cce99b
nirrvana <wlwhs91@gmail.com>
2020-02-17 20:30:50 +0900

    Implement css in sign in page

--- src/components/company/CompanySignIn.js
+++ src/components/company/CompanySignIn.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import 'antd/dist/antd.css';
-import { Form, Input, Tooltip, Icon, Button, AutoComplete } from 'antd';
 import { postCompanySignInData } from '../../redux/api';
+import { Form, Input, Tooltip, Icon, Button, AutoComplete } from 'antd';
+import 'antd/dist/antd.css';
+import TabNoBtn from '../../pages/TabNoBtn';
 
 class CompanySignIn extends Component {
   state = {
@@ -48,55 +49,69 @@ class CompanySignIn extends Component {
     };
 
     return (
-      <Form {...formItemLayout} onSubmit={this.handleSubmit}>
-        <Form.Item
-          label={
-            <span>
-              company code&nbsp;
-              <Tooltip title="What's your company code?">
-                <Icon type="question-circle-o" />
-              </Tooltip>
-            </span>
-          }
-        >
-          {getFieldDecorator('company_code', {
-            rules: [
-              {
-                required: true,
-                message: 'Please input your company code!',
-                whitespace: true,
-              },
-            ],
-          })(<Input />)}
-        </Form.Item>
-        <Form.Item label="username">
-          {getFieldDecorator('username', {
-            rules: [{ required: true, message: 'Please input username!' }],
-          })(
-            <AutoComplete>
-              <Input />
-            </AutoComplete>,
-          )}
-        </Form.Item>
-        <Form.Item label="Password" hasFeedback>
-          {getFieldDecorator('password', {
-            rules: [
-              {
-                required: true,
-                message: 'Please input your password!',
-              },
-              {
-                validator: this.validateToNextPassword,
-              },
-            ],
-          })(<Input.Password />)}
-        </Form.Item>
-        <Form.Item {...tailFormItemLayout}>
-          <Button type="primary" htmlType="submit">
-            Register
-          </Button>
-        </Form.Item>
-      </Form>
+      <div>
+        <TabNoBtn></TabNoBtn>
+        <div className="cl_SignInComponent">
+          <Form
+            {...formItemLayout}
+            onSubmit={this.handleSubmit}
+            className="cl_SignInForm"
+          >
+            <div className="cl_SignIn_header cl_SignIn_Company_header">Company Sign in</div>
+            <Form.Item
+              label={
+                <span>
+                  company code&nbsp;
+                  <Tooltip title="What's your company code?">
+                    <Icon type="question-circle-o" />
+                  </Tooltip>
+                </span>
+              }
+            >
+              {getFieldDecorator('company_code', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input your company code!',
+                    whitespace: true,
+                  },
+                ],
+              })(<Input className="cl_SignIn_Company_Input" />)}
+            </Form.Item>
+            <Form.Item label="username">
+              {getFieldDecorator('username', {
+                rules: [{ required: true, message: 'Please input username!' }],
+              })(
+                <AutoComplete>
+                  <Input className="cl_SignIn_Company_Input" />
+                </AutoComplete>,
+              )}
+            </Form.Item>
+            <Form.Item label="password" hasFeedback>
+              {getFieldDecorator('password', {
+                rules: [
+                  {
+                    required: true,
+                    message: 'Please input your password!',
+                  },
+                  {
+                    validator: this.validateToNextPassword,
+                  },
+                ],
+              })(<Input.Password className="cl_SignIn_Company_Input" />)}
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="cl_SignIn_Company_Login_Btn"
+              >
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </div>
+      </div>
     );
   }
 }
--- src/components/signin/NormalSignIn.js
+++ src/components/signin/NormalSignIn.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { postSignInData } from '../../redux/api';
 import { signin } from '../../redux/action';
 import { Link, Redirect } from 'react-router-dom';
+import TabNoBtn from '../../pages/TabNoBtn';
 import { Form, Icon, Input, Button, Checkbox, message } from 'antd';
 
 class NormalSignInForm extends React.Component {
@@ -34,52 +35,67 @@ class NormalSignInForm extends React.Component {
       return <Redirect to="/" />;
     } else {
       return (
-        <Form onSubmit={this.handleSubmit} className="login-form">
-          <Form.Item>
-            {getFieldDecorator('email', {
-              rules: [{ required: true, message: 'Please input your email!' }],
-            })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                placeholder="email"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('password', {
-              rules: [
-                { required: true, message: 'Please input your Password!' },
-              ],
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
-                }
-                type="password"
-                placeholder="Password"
-              />,
-            )}
-          </Form.Item>
-          <Form.Item>
-            {getFieldDecorator('remember', {
-              valuePropName: 'checked',
-              initialValue: true,
-            })(<Checkbox>Remember me</Checkbox>)}
-            <a className="login-form-forgot" href="">
-              Forgot password
-            </a>
-            <Button
-              type="primary"
-              htmlType="submit"
-              className="login-form-button"
-            >
-              Log in
-            </Button>
-            Or <Link to="/SignUp">register now!</Link>
-          </Form.Item>
-        </Form>
+        <div>
+          <TabNoBtn></TabNoBtn>
+          <div className="cl_SignInComponent">
+            <Form onSubmit={this.handleSubmit} className="cl_SignInForm">
+              <div className="cl_SignIn_header">Sign in</div>
+              <Form.Item>
+                {getFieldDecorator('email', {
+                  rules: [
+                    { required: true, message: 'Please input your email!' },
+                  ],
+                })(
+                  <Input
+                    prefix={
+                      <Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />
+                    }
+                    placeholder="email"
+                    className="cl_SignIn_DevInput"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('password', {
+                  rules: [
+                    { required: true, message: 'Please input your Password!' },
+                  ],
+                })(
+                  <Input
+                    prefix={
+                      <Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />
+                    }
+                    type="password"
+                    placeholder="Password"
+                    className="cl_SignIn_DevInput"
+                  />,
+                )}
+              </Form.Item>
+              <Form.Item>
+                {getFieldDecorator('remember', {
+                  valuePropName: 'checked',
+                  initialValue: true,
+                })(
+                  <Checkbox className="cl_SignIn_Checkbox">
+                    Remember me
+                  </Checkbox>,
+                )}
+                <Link to="">Forgot password</Link>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button cl_SignIn_Login_Btn"
+                >
+                  Login
+                </Button>
+                <br />
+                <Link to="/SignUp" className="cl_SignIn_Register">
+                  register now!
+                </Link>
+              </Form.Item>
+            </Form>
+          </div>
+        </div>
       );
     }
   }
