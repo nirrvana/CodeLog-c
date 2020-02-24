c8f4603a432bd73b129ec5fe25d5bb29bcc897e2
nirrvana <wlwhs91@gmail.com>
2020-02-24 13:25:54 +0900

    Fix mypage element

--- src/pages/developer/MyPage2.js
+++ src/pages/developer/MyPage2.js
@@ -1,102 +1,3 @@
-// import React, { Component } from 'react';
-// import { getMyPageData } from '../../redux/api';
-// //* component
-// import MypageTab from '../../pages/TabMypage';
-// // * css
-// import { Upload, Icon, message } from 'antd';
-// import fakedata from '../../fakedata';
-
-// class MyPage2 extends Component {
-//   state = {
-//     post_count: null,
-//     tag_count: null,
-//     recommended_companies: [...fakedata],
-//   };
-
-//   componentDidMount() {
-//     getMyPageData()
-//       .then(({ data: { post_count, tags, completion } }) =>
-//         this.setState({ post_count, tag_count: tags.length, completion }),
-//       )
-//       .catch((err) =>
-//         this.setState({
-//           post_count: 'Fail to receive post count',
-//           tag_count: 'Fail to receive tag count',
-//         }),
-//       );
-//   }
-
-//   render() {
-//     const props = {
-//       name: 'file',
-//       multiple: true,
-//       action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
-//       onChange(info) {
-//         const { status } = info.file;
-//         if (status !== 'uploading') {
-//         }
-//         if (status === 'done') {
-//           message.success(`${info.file.name} file uploaded successfully.`);
-//         } else if (status === 'error') {
-//           message.error(`${info.file.name} file upload failed.`);
-//         }
-//       },
-//     };
-//     const { Dragger } = Upload;
-//     const {
-//       post_count,
-//       tag_count,
-//       completion,
-//       recommended_companies,
-//     } = this.state;
-//     return (
-//       <div>
-//         <MypageTab />
-//         <div className="cl_Mypage_Tags">
-//           <div className="cl_Count_Wrapper">
-//             <div className="cl_Count_Name">Tag and Post</div>
-//             <div className="cl_Count_element">{post_count}Posts</div>
-//             <div className="cl_Count_element">{tag_count}Tags</div>
-//             <div className="cl_Count_element">{completion} 수료</div>
-//           </div>
-//         </div>
-//         <div className="cl_Document_Upload_Wrapper">
-//           <div className="cl_Document_Upload_Header">Resume and portfolio</div>
-//           <div className="cl_Document_Upload_container">
-//             <Dragger {...props}>
-//               <p className="ant-upload-drag-icon">
-//                 <Icon type="inbox" />
-//               </p>
-//               <p className="ant-upload-text">
-//                 Click or drag resume or portfolio to this area to upload
-//               </p>
-//               <p className="ant-upload-hint">
-//                 Support for a single or bulk upload. Strictly prohibit from
-//                 uploading company data or other band files
-//               </p>
-//             </Dragger>
-//           </div>
-//         </div>
-//         <div className="cl_Company_Recommend cl_CompanyMyPage_Set">
-//           <div className="cl_Recommended_Header">
-//             Recommended Company for you
-//           </div>
-//           <div className="cl_Company_Recommended_List">
-//             {recommended_companies.map((recommended_company, index) => (
-//               <div className="cl_Company_Recommended_Element" index={index}>
-//                 <span>{recommended_company.id}</span>
-//                 <span>{recommended_company.body}</span>
-//               </div>
-//             ))}
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default MyPage2;
-
 import React, { Component } from 'react';
 import { getMyPageData } from '../../redux/api';
 //* component
@@ -107,15 +8,27 @@ import fakedata from '../../fakedata';
 
 class MyPage2 extends Component {
   state = {
+    username: '',
     post_count: null,
     tag_count: null,
+    certificate: '',
+    tags: [],
     recommended_companies: [...fakedata],
+    recommended_tags: ['연봉삼천', '야근없음', '리액트', '프론트'],
   };
 
   componentDidMount() {
     getMyPageData()
-      .then(({ data: { post_count, tags, completion } }) =>
-        this.setState({ post_count, tag_count: tags.length, completion }),
+      .then(
+        ({ data: { username, post_count, tag_count, certificate, tags } }) => {
+          this.setState({
+            username,
+            post_count,
+            tag_count,
+            certificate,
+            tags,
+          });
+        },
       )
       .catch((err) =>
         this.setState({
@@ -145,7 +58,7 @@ class MyPage2 extends Component {
     const {
       post_count,
       tag_count,
-      completion,
+      certificate,
       recommended_companies,
     } = this.state;
     return (
@@ -170,7 +83,7 @@ class MyPage2 extends Component {
             </div>
             <div className="cl_Dev_Mypage_Tag_Post_Set">
               <div className="cl_Dev_Mypage_Tag_Post_Datas">
-                <div className="cl_Dev_Mypage_Tag_Post_Data">{completion}?</div>
+                <div className="cl_Dev_Mypage_Tag_Post_Data">{certificate}</div>
                 수료
               </div>
             </div>
